5f978f8bbe6d6e87f1d98c5192a8a4df65780eb1
Jose Escribano <jescribanomora@gmail.com>
2024-03-22 14:54:26 +0100

    feat: create Card and CardList components

--- src/components/CardsList.tsx
+++ src/components/CardsList.tsx
@@ -1,3 +1,31 @@
+import { Card } from "./Card";
+import { useFetchProductList } from "@/hooks/useFetchProductList";
+import { LoadMoreButton } from "@/components/LoadMoreButton";
+import { Message } from "@/components/Message";
+
 export const CardsList = () => {
-    return <div>CardsList</div>;
+    const { productList, endReached, loadMoreProducts } = useFetchProductList();
+
+    return (
+        <>
+            <h2>Products list</h2>
+
+            {productList?.products && (
+                <div className="grid grid-cols-2 gap-4 my-5">
+                    {productList.products.map((product: Product) => (
+                        <Card key={product.id} {...product} />
+                    ))}
+                </div>
+            )}
+
+            {endReached && (
+                <Message text="There is no more products" type="error" />
+            )}
+
+            <LoadMoreButton
+                loadMoreProducts={loadMoreProducts}
+                endReached={endReached}
+            />
+        </>
+    );
 };
