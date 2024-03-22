c5ae23c2a6ccca737d0c32936f80b855d5e98e67
Jose Escribano <jescribanomora@gmail.com>
2024-03-22 12:14:05 +0100

    feat: removed unused code from LoadMoreApp

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import LoadMoreApp from "./LoadMoreApp.tsx";
+import { LoadMoreApp } from "./LoadMoreApp.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
