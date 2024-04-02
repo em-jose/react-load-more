210226e81a958ff3abb1a3793eb17abc7d072819
Jose Escribano <jescribanomora@gmail.com>
2024-04-02 17:01:17 +0200

    feat: style header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,43 @@
-import { Link } from "react-router-dom";
-import { ModeToggle } from "./ui/mode-toggle";
+import { NavLink } from "react-router-dom";
+import { twMerge } from "tailwind-merge";
+// import { ModeToggle } from "./ui/mode-toggle";
 
 export const Header = () => {
     return (
         <header className="p-5">
             <div className="flex justify-center">
-                <h1 className="text-2xl uppercase">Load More App</h1>
+                <h1 className="text-3xl uppercase">Load More App</h1>
             </div>
-            <div className="flex gap-2 justify-center items-center">
-                <Link to="/button" className="uppercase underline">
+            <div className="flex gap-2 justify-center items-center flex-wrap mt-3">
+                <NavLink
+                    to="/button"
+                    className={({ isActive }) =>
+                        twMerge(
+                            "font-medium text-blue-500 hover:underline",
+                            isActive && "underline"
+                        )
+                    }
+                >
                     Load more with button
-                </Link>
-                <Link to="/infinite" className="uppercase underline">
+                </NavLink>
+                <NavLink
+                    to="/infinite"
+                    className={({ isActive }) =>
+                        twMerge(
+                            "font-medium text-blue-500 hover:underline",
+                            isActive && "underline"
+                        )
+                    }
+                >
                     Infinite scroll
-                </Link>
+                </NavLink>
                 <a
                     href="https://github.com/em-jose/react-load-more"
                     target="_blank"
-                    className="uppercase underline"
+                    className="font-medium text-blue-500 hover:underline"
                 >
                     Github
                 </a>
-                <div className="absolute top-0 right-0 m-5">
-                    <ModeToggle />
-                </div>
             </div>
         </header>
     );
