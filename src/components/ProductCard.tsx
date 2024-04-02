b417262002c4031a71a23e10e36efee1c58b67e9
Jose Escribano <jescribanomora@gmail.com>
2024-04-02 18:17:06 +0200

    feat: style product card component

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,42 +2,36 @@ import "animate.css";
 import {
     Card,
     CardContent,
-    CardDescription,
     CardFooter,
     CardHeader,
     CardTitle,
 } from "@/components/ui/card";
+import { Rating } from "./Rating";
 
 export const ProductCard = (product: Product) => {
+    const originalPrice = Math.floor(
+        product.price / (1 - product.discountPercentage / 100)
+    );
+
     return (
         <>
             <Card>
-                <CardHeader>
-                    <CardTitle>
-                        <div className="text-center">{product.title}</div>
-                    </CardTitle>
-                    <CardDescription>
-                        <div className="flex justify-center">
-                            <img
-                                src={product.thumbnail}
-                                alt=""
-                                className="rounded max-h-48"
-                            />
-                        </div>
-                    </CardDescription>
-                </CardHeader>
-                <CardContent>
+                <CardHeader className="pb-0">
                     <div className="flex justify-center">
-                        <ul>
-                            <li>Discount: {product?.discountPercentage}%</li>
-                            <li>Price: {product.price}€</li>
-                        </ul>
+                        <img
+                            src={product.thumbnail}
+                            alt=""
+                            className="rounded max-h-36"
+                        />
                     </div>
+                    <CardTitle className="text-lg">{product.title}</CardTitle>
+                </CardHeader>
+                <CardContent className="py-0">
+                    <Rating rating={product.rating} />
                 </CardContent>
-                <CardFooter>
-                    <div className="text-center w-full">
-                        {product.description}
-                    </div>
+                <CardFooter className="flex gap-2">
+                    <span className="line-through">{originalPrice}€</span>
+                    <span>{product.price}€</span>
                 </CardFooter>
             </Card>
         </>
